f2f3519917cf023c7582230502c8768e9a8f15a5
suryasai <suryasaichanti@gmail.com>
2023-02-28 11:19:26 +0530

    redux setup

---
+++ src/utils/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import Appslice from "./Appslice";
+const store = configureStore({
+    reducer:{
+        app:Appslice,
+    }
+});
+export default store;
