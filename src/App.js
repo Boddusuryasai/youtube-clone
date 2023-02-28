1c974f490e8372e512383a5652feba829a99cc60
suryasai <suryasaichanti@gmail.com>
2023-02-28 10:39:50 +0530

    home page layout done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Body from './Components/Body';
+import Header from './Components/Header';
+import MainContainer from './Components/MainContainer';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header/>
+     <Body/>
     </div>
   );
 }
